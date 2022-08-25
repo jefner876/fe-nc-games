a209ae503ca7c71ec6f615fc8ee0347440d56a17
jefner876 <jefner876@hotmail.com>
2022-08-25 13:08:18 +0100

    add age factor

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,8 +1,10 @@
 import styles from "../modules/CommentCard.module.css";
-const { commentCard, commentBody, commentCardHeader } = styles;
+import { secondsToAge } from "../utils/seconds-to-age";
+const { commentCard, commentBody, commentCardHeader, postAge } = styles;
 
 export const CommentCard = ({ comment }) => {
-  const { body, votes, author } = comment;
+  const { body, votes, author, created_at } = comment;
+  const age = secondsToAge((new Date() - new Date(created_at)) / 1000);
   return (
     <section className={commentCard}>
       <section className={commentCardHeader}>
@@ -10,6 +12,7 @@ export const CommentCard = ({ comment }) => {
         <p>Score: {votes}</p>
       </section>
       <p className={commentBody}>{body}</p>
+      <p className={postAge}>{age}</p>
     </section>
   );
 };
