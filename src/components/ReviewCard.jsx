f4527017db5ec2bb6592914a96765c3d52bdea1d
jefner876 <jefner876@hotmail.com>
2022-08-25 09:17:31 +0100

    improve reviews links

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "../modules/ReviewCard.module.css";
-const { reviewHeading, reviewCard, reviewImg, categoryText } = styles;
+const { reviewHeading, reviewLinks, reviewCard, reviewImg, categoryText } =
+  styles;
 
 export const ReviewCard = ({ review }) => {
   const { category, review_img_url, designer, votes, title, review_id } =
@@ -8,13 +9,13 @@ export const ReviewCard = ({ review }) => {
 
   return (
     <section className={reviewCard}>
-      <Link to={`/review/${review_id}`}>
+      <Link className={reviewLinks} to={`/review/${review_id}`}>
         <img className={reviewImg} src={review_img_url} alt={title} />
+        <h3 className={reviewHeading}>{title}</h3>
+        <p className={categoryText}>{category}</p>
+        <p>Designed by: {designer} </p>
+        <p>Score: {votes}</p>
       </Link>
-      <h3 className={reviewHeading}>{title}</h3>
-      <p className={categoryText}>{category}</p>
-      <p>Designed by: {designer} </p>
-      <p>Score: {votes}</p>
     </section>
   );
 };
